57827db7bffa1beb1017994b0eec8185d945baa9
onionmccabbage <onionmccabbage@live.com>
2023-06-06 16:20:06 +0100

    sample react app using ts

---
+++ 06_react/my-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <article>
+        <section>
+          <h3>It Works!!!</h3>
+        </section>
+      </article>
+    </div>
+  );
+}
+
+export default App;
